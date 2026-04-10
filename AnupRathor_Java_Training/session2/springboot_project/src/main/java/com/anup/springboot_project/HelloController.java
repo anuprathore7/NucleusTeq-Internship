7c7a4473aa66014444d2f6a1668b00d4986851e0
Anup Rathore <anuprathore1669@gmail.com>
2026-04-10 10:30:48 +0530

    Initial Project Structure added with one Controller

---
+++ AnupRathor_Java_Training/session2/springboot_project/src/main/java/com/anup/springboot_project/HelloController.java
@@ -0,0 +1,12 @@
+package com.anup.springboot_project;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String sayHello(){
+        return "message : hello jeee";
+    }
+}
