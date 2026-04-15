f8506eacf546751542c5edf7e0ee0b3449c84f42
Anup Rathore <anuprathore1669@gmail.com>
2026-04-15 11:11:57 +0530

    Added Initial Folder structure of Spring_and_rest_assignment

---
+++ AnupRathor_Java_Training/session3/spring_and_rest_assignment/src/main/java/com/anup/spring_and_rest_assignment/SpringAndRestAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.anup.spring_and_rest_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndRestAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndRestAssignmentApplication.class, args);
+	}
+
+}
