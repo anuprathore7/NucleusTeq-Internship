33c01a22a14cc7280641b982141c373ef48a5cea
Anup Rathore <anuprathore1669@gmail.com>
2026-04-18 14:58:44 +0530

    Created NotificationService File for notification

--- AnupRathor_Java_Training/session4/spring_advance_assignment/src/main/java/com/anup/spring_advance_assignment/service/NotificationService.java
+++ AnupRathor_Java_Training/session4/spring_advance_assignment/src/main/java/com/anup/spring_advance_assignment/service/NotificationService.java
@@ -1,5 +1,15 @@
 package com.anup.spring_advance_assignment.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
 public class NotificationService {
-    
+
+    private static final Logger logger = LoggerFactory.getLogger(NotificationService.class);
+
+    public void sendNotification(String message) {
+        logger.info("Notification sent: {}", message);
+    }
 }
