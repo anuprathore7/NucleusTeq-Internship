c189729016bf93f90c1f09e941ec476f95ca1562
Anup Rathore <anuprathore1669@gmail.com>
2026-04-18 21:14:22 +0530

    feat : Added unit testing setup with JUnit and Mockito

---
+++ AnupRathor_Java_Training/session4/spring_advance_assignment/src/test/java/com/anup/spring_advance_assignment/TodoServiceTest.java
@@ -0,0 +1,39 @@
+
+package com.anup.spring_advance_assignment;
+
+import com.anup.spring_advance_assignment.service.NotificationService;
+import com.anup.spring_advance_assignment.service.TodoServiceImpl;
+import com.anup.spring_advance_assignment.dto.TodoDTO;
+import com.anup.spring_advance_assignment.entity.Status;
+import com.anup.spring_advance_assignment.entity.Todo;
+import com.anup.spring_advance_assignment.exception.InvalidStatusException;
+import com.anup.spring_advance_assignment.exception.ResourceNotFoundException;
+import com.anup.spring_advance_assignment.repository.TodoRepository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class TodoServiceTest {
+
+    @Mock
+    private TodoRepository repository;
+
+    @Mock
+    private NotificationService notificationClient;
+
+    @InjectMocks
+    private TodoServiceImpl service;
+
+    
+}
