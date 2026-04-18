986ba60672a95386033b7d2e8b361d86f01c2cc1
Anup Rathore <anuprathore1669@gmail.com>
2026-04-18 15:11:21 +0530

    feat: Added SLF4J logging in Controller layer

--- AnupRathor_Java_Training/session4/spring_advance_assignment/src/main/java/com/anup/spring_advance_assignment/controller/TodoController.java
+++ AnupRathor_Java_Training/session4/spring_advance_assignment/src/main/java/com/anup/spring_advance_assignment/controller/TodoController.java
@@ -1,6 +1,7 @@
 package com.anup.spring_advance_assignment.controller;
 import java.util.*;
 
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,12 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 import com.anup.spring_advance_assignment.dto.TodoDTO;
 import com.anup.spring_advance_assignment.service.TodoService;
 
+import jakarta.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 
 @RestController
 @RequestMapping("/todos")
 public class TodoController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TodoController.class);
     private final TodoService service;
 
     public TodoController(TodoService service){
@@ -25,27 +32,33 @@ public class TodoController {
     }
 
     @PostMapping()
-    public String createTodo(@RequestBody TodoDTO dto){
+    public String createTodo(@RequestBody @Valid TodoDTO dto){
+        logger.info("Received request to create TODO with title: {}", dto.getTitle());
         return service.createTodo(dto);
     }
 
     @GetMapping()
     public List<TodoDTO> getAllTodos(){
+        logger.info("Received request to fetch all TODOs");
         return service.getAllTodos();
     }
 
     @GetMapping("/{id}")
     public TodoDTO geTodoDTO(@PathVariable Long id){
+        logger.info("Received request to fetch TODO with ID: {}", id);
+
         return service.getTodoById(id);
     }
     
     @PutMapping("/{id}")
-    public String updateTodo(@PathVariable Long id , @RequestBody TodoDTO dto){
+    public String updateTodo(@PathVariable Long id , @RequestBody @Valid TodoDTO dto){
+        logger.info("Received request to update TODO with ID: {}", id);
         return service.updateTodo(id, dto);
     }
 
     @DeleteMapping("/{id}")
     public String deleteTodo(@PathVariable Long id){
+        logger.info("Received request to delete TODO with ID: {}", id);
         return service.deleteTodo(id);
     }
 
